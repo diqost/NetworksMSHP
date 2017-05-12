bbddb8178314c3e2e57ae2eeb0b95dc933923829
diqost <sapozhkov2008@yandex.ru>
2017-05-12 20:12:14 +0300

    latest

--- server_networks.py
+++ server_networks.py
@@ -5,7 +5,7 @@ import threading
 err_msg = json.dumps({ 'status': 'bad', 
 					   'reason': 'wrong msg format'})
 ok_msg = json.dumps({ 'status': 'ok'})
-
+clients = []
 
 
 class ThreadedServer(object):
@@ -18,32 +18,34 @@ class ThreadedServer(object):
     def listen(self):
         self.sock.listen(5)
         while True:
-            client, address = self.sock.accept()
-            print(client,address);
-            client.settimeout(60)
-            threading.Thread(target = self.listenToClient,args = (client,address)).start()
+            conn, address = self.sock.accept()
+            print(conn,address);
+            conn.settimeout(60)
+            data = json.loads(conn.recv(1024).decode('utf-8'))
+            if data['type'] == 'server':
+                if len(data['login']) > 0:
+                    conn.send(bytes(ok_msg,'utf-8'))
+                else: 
+                    conn.send(bytes(err_msg,'utf-8'))
+            else:
+                conn.send(bytes(err_msg,'utf-8'))
+            clients.append((conn,data['login']))
+            threading.Thread(target = self.listenToClient,args = (conn,address)).start()
 
     def listenToClient(self, conn, address):
         size = 1024
-        data = json.loads(conn.recv(1024).decode('utf-8'))
-        if data['type'] == 'server':
-            if len(data['login']) > 0:
-                conn.send(bytes(ok_msg,'utf-8'))
-            else: 
-                conn.send(bytes(err_msg,'utf-8'))
-        else:
-            conn.send(bytes(err_msg,'utf-8'))
-
         while True:
             try:
                 data = json.loads(conn.recv(1024).decode('utf-8'))
                 print(data)
-                if data['type'] == 'server':
-                	print("smth wrong")
-                	conn.send(bytes(err_msg,'utf-8'))
+                if data['type'] == 'msg':
+                	conn.send(bytes(json.dumps({ 'type': 'msg', 'from': data['from'],'msg': data['msg'] }), 'utf-8'))
                 else:
-                    raise error('Client disconnected')
-                    conn.close()
+                    if data['type'] == 'users':
+                        conn.send(bytes(json.dumps({ 'type': 'users', 'users': clients[0].login }), 'utf-8'))
+                    else:
+                        raise error('Client disconnected')
+                        conn.close()
 
             except:
                 conn.close()
@@ -51,4 +53,4 @@ class ThreadedServer(object):
 
 
 
-ThreadedServer('192.168.1.121',9087).listen()
+ThreadedServer('192.168.1.121',9013).listen()
